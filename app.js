3ed22bd9ec0f6e60dd2cfe5ea7e7445380b21fdf
邹亚 <zouya@corp.netease.com>
2016-01-08 17:38:34 +0800

    commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var express = require('express'),
 
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3001);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
